0a45b2b17b2f664fdc5d1447d83e1be1ac3a660b
Nick Santos <nickrsan@users.noreply.github.com>
2016-02-09 11:18:14 -0800

    Removing exec statement in setup.py, converting to import
    exec statement was added without comment, converting to standard import
    - nothing particularly fancy here.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-with open('arcpy_metadata/version.py') as fin: exec(fin.read())
+from arcpy_metadata import version
 
 setup(name="arcpy_metadata",
-	version=__version__,
+	version=version.__version__,
 	description="Python metadata editing classes for ArcGIS feature classes",
 	#scripts=[],
 	packages=['arcpy_metadata',],
-	author="Nick Santos, with contributions from other CWS staff",
+	author=version.__author__,
 	author_email="nrsantos@ucdavis.edu",
 	url='https://github.com/ucd-cws/arcpy_metadata',
 )
