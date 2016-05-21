1b13cfc3585498e65abedf198e6f70c2669bd6a1
Nick Santos <nickrsan@users.noreply.github.com>
2016-05-20 17:00:51 -0700

    Another minor update to setup to change description to long_description

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ from arcpy_metadata import version
 
 setup(name="arcpy_metadata",
 	version=version.__version__,
-	description="Python metadata editing classes for ArcGIS feature classes. WARNING: Version 0.4 deprecates the .get() and .set() methods used in prior versions of arcpy_metadata. you can now set values using normal Python, such as by setting metadata.title = \"feature class title\"",
+	description="Python metadata editing classes for ArcGIS feature classes.",
+	long_description="""WARNING: Version 0.4 deprecates the .get() and .set() methods used in prior versions of arcpy_metadata. you can now set values using normal Python, such as by setting metadata.title = \"feature class title\".
+	
+	See all metadata elements at https://github.com/ucd-cws/arcpy_metadata""",
 	#scripts=[],
 	packages=['arcpy_metadata',],
 	author=version.__author__,
