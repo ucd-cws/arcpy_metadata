c9a143d29ceffed6c17ee26a18c7d0f3d1694c1e
lali <li.lawrly@gmail.com>
2016-07-19 16:10:51 -0700

    update init file

--- arcpy_metadata/__init__.py
+++ arcpy_metadata/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
-from version import __author__
-from version import __version__
+from arcpy_metadata.version import __author__
+from arcpy_metadata.version import __version__
 from metadata_editor import MetadataEditor
 
 
