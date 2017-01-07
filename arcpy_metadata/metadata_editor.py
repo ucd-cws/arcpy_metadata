3ac5d55f16bf7137eab62f7d267932dcd6672a9e
Thomas Maschler <tmaschler@wri.org>
2017-01-07 09:48:37 -0500

    Avoid using "non-Pythonic" variable names

--- arcpy_metadata/metadata_editor.py
+++ arcpy_metadata/metadata_editor.py
@@ -36,8 +36,8 @@ except ImportError:
         print("WARNING: {0:s}".format(log_string))
 
 
-installDir = arcpy.GetInstallInfo("desktop")["InstallDir"]
-xslt = os.path.join(installDir, r"Metadata\Stylesheets\gpTools\exact copy of.xslt")
+install_dir = arcpy.GetInstallInfo("desktop")["InstallDir"]
+xslt = os.path.join(install_dir, r"Metadata\Stylesheets\gpTools\exact copy of.xslt")
 metadata_temp_folder = arcpy.env.scratchFolder  # a default temp folder to use - settable by other applications so they can set it once
 
 
